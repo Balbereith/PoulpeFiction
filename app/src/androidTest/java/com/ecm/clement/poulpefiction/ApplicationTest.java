4e079ef08d28a9b38d60cd21e373ab6728012248
Dechy <clement.dechy@gmail.com>
2016-02-16 12:54:22 +0100

    first commit

---
+++ app/src/androidTest/java/com/ecm/clement/poulpefiction/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ecm.clement.poulpefiction;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
