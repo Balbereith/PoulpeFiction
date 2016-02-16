5d217eec6e103199876b3e691e0d437efe9dc4e2
Dechy <clement.dechy@gmail.com>
2016-02-16 16:44:54 +0100

    Creation of the 4 main activities

--- app/src/main/java/com/ecm/clement/poulpefiction/SettingsActivity.java
+++ app/src/main/java/com/ecm/clement/poulpefiction/SettingsActivity.java
@@ -3,24 +3,23 @@ package com.ecm.clement.poulpefiction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity
+public class SettingsActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -61,20 +60,20 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-    if (id == R.id.nav_alAffiche) {
-            Intent monIntent = new Intent(MainActivity.this,AlafficheActivity.class);
+        if (id == R.id.nav_alAffiche) {
+            Intent monIntent = new Intent(SettingsActivity.this,AlafficheActivity.class);
             startActivity(monIntent);
 
         } else if (id == R.id.nav_prochainement) {
-            Intent monIntent = new Intent(MainActivity.this,ProchainnementActivity.class);
+            Intent monIntent = new Intent(SettingsActivity.this,ProchainnementActivity.class);
             startActivity(monIntent);
 
         } else if (id == R.id.nav_evennements) {
-            Intent monIntent = new Intent(MainActivity.this,EventActivity.class);
+            Intent monIntent = new Intent(SettingsActivity.this,EventActivity.class);
             startActivity(monIntent);
 
         } else if (id == R.id.nav_preferences) {
-            Intent monIntent = new Intent(MainActivity.this,SettingsActivity.class);
+            Intent monIntent = new Intent(SettingsActivity.this,SettingsActivity.class);
             startActivity(monIntent);
 
         }
